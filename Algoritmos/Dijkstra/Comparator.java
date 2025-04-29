644024cf08f102492239bd687ee703d4149e065a
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-04-29 00:02:24 +0000

    :bulb: update: update structure of repository

--- Algoritmos/Dijkstra/Comparator.java
+++ Algoritmos/Dijkstra/Comparator.java
@@ -2,7 +2,7 @@ package Dijkstra;
 
 import java.util.Comparator;
 
-public class MyComparator implements Comparator<Integer> {
+public class Comparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
         return o1.compareTo(o2);
